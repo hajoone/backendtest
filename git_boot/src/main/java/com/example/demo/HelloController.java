70a64c73152d5a00d0035ede46c39e2043df45c0
hajoone <124555061+hajoone@users.noreply.github.com>
2023-05-30 17:32:36 +0900

    Update HelloController.java

--- git_boot/src/main/java/com/example/demo/HelloController.java
+++ git_boot/src/main/java/com/example/demo/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 	@RequestMapping("/helloboot")
 	public ModelAndView helloboot() {
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("model", "스프링부트를 시작합니다.");
+		mv.addObject("model", "스프링부트를 시작합니다.(깃헙)");
 		mv.setViewName("hello");
 		return mv;
 	}
